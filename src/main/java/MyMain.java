fe4c99a10b53f17b5dfeadb1da4b053542960d82
Tavi Greenfield <tavi@elyandpilar.com>
2021-09-10 15:04:46 -0700

    Wrote math with loops code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,22 +4,38 @@ public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int product=1;
+        while (x>0) {
+            product=product*x;
+            x--;
+        }
+        return product;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double e=0;
+        int x=0;
+        while (Math.abs(e-(Math.E))>0.00001) {
+            e = e + (1/factorial(x));
+            x++;
+        }
+        return e;
     }
 
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double guess=x/2;
+        double betterGuess= guess+1;
+        boolean close=false;
+        while (!close) {
+            betterGuess=(guess + x/guess) / 2;
+            close = (Math.abs(guess-betterGuess)<0.00001);
+            guess=betterGuess;
+        }
+        return betterGuess;
     }
     
     
@@ -27,7 +43,10 @@ public class MyMain {
         Scanner scan = new Scanner(System.in);
 
         // YOUR CODE HERE
-
+        System.out.println("enter a positive number");
+        double num = scan.nextDouble();
+        System.out.println("the square root of " + num + "is " + babylonian(num));
+        System.out.println("The value of e is roughly " + calculateE());
 
         
 
